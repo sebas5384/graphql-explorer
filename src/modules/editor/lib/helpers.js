f122f5dfed3a15ae26e1ecd828e9ae4f8bb33bc6
Sebastian <sebas@taller.net.br>
2018-03-04 21:06:14 -0300

    Added transformation of the name of types.

--- src/modules/editor/lib/helpers.js
+++ src/modules/editor/lib/helpers.js
@@ -1,3 +1,6 @@
+import * as R from 'ramda'
+import { toCamelCase, toStudlyCaps } from 'strman'
+
 export const fieldIsInvalid = field => {
   const brackets = field.type.match(/\[|\]/g)
   if (brackets && brackets.length < 2) {
@@ -5,3 +8,11 @@ export const fieldIsInvalid = field => {
   }
   return false
 }
+
+export const isNodeModel = R.propSatisfies(R.equals('model'), 'type')
+
+export const normalizeNodeName = R.ifElse(
+  isNodeModel,
+  R.over(R.lensProp('name'), toStudlyCaps),
+  R.over(R.lensProp('name'), toCamelCase)
+)
