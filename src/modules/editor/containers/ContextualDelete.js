8faf456a48d7558c58825fa8140009d6d378c152
Walmyr <wlsf82@gmail.com>
2020-09-27 23:56:02 +0200

    Use hooks on contextual delete component
    Instead of HOCs.
    
    Closes https://github.com/sebas5384/graphql-explorer/issues/21.

--- src/modules/editor/containers/ContextualDelete.js
+++ src/modules/editor/containers/ContextualDelete.js
@@ -1,44 +1,55 @@
-import ContextualDelete from '../components/ContextualDelete'
-import { compose, withHandlers, branch, renderNothing, lifecycle } from 'recompose'
-import { connect } from 'react-redux'
+import React, { useCallback, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import ContextualDelete from "../components/ContextualDelete";
 import {
   updateContextualDelete,
   getSelectedNode,
-  markNodeReadyToDelete
-} from '../store'
+  markNodeReadyToDelete,
+} from "../store";
+
+const ContextualDeleteContainer = (props) => {
+  const state = useSelector(mapStateToProps);
+  const dispatch = useDispatch();
+
+  const handleOnMouseLeave = useCallback(() => {
+    dispatch(updateContextualDelete({ isActive: false }));
+  }, [dispatch]);
+
+  const { name, type } = state.selectedNode || {};
+  const handleOnClick = useCallback(() => {
+    dispatch(updateContextualDelete({ isActive: false }));
+    // Avoid recursive when is a relation node.
+    dispatch(markNodeReadyToDelete({ name, recursive: type !== "relation" }));
+  }, [name, type, dispatch]);
+
+  const { isActive } = state;
+
+  useEffect(() => {
+    isActive
+      ? (document.body.style.cursor = "pointer")
+      : (document.body.style.cursor = "default");
+
+    return () => (document.body.style.cursor = "default");
+  }, [isActive]);
+
+  if (!state.isActive) return null;
+
+  return (
+    <ContextualDelete
+      {...props}
+      {...state}
+      handleOnMouseLeave={handleOnMouseLeave}
+      handleOnClick={handleOnClick}
+    />
+  );
+};
 
 const mapStateToProps = ({ contextualDelete, nodes }) => {
   return {
     ...contextualDelete,
-    selectedNode: getSelectedNode(nodes)
-  }
-}
+    selectedNode: getSelectedNode(nodes),
+  };
+};
 
-const handleOnMouseLeave = ({ isActive, dispatch, selectedNode }) => event => {
-  dispatch(updateContextualDelete({ isActive: false }))
-  document.body.style.cursor = 'default'
-}
-
-function componentWillReceiveProps(nextProps) {
-  if (nextProps.isActive) {
-    document.body.style.cursor = 'pointer'
-  }
-  return nextProps
-}
-
-const handleOnClick = ({ dispatch, selectedNode: { name, type } }) => () => {
-  dispatch(updateContextualDelete({ isActive: false }))
-  // Avoid recursive when is a relation node.
-  dispatch(markNodeReadyToDelete({ name, recursive: type !== 'relation' }))
-}
-
-export default compose(
-  connect(mapStateToProps),
-  withHandlers({ handleOnMouseLeave, handleOnClick }),
-  lifecycle({ componentWillReceiveProps, }),
-  branch(
-    ({ isActive }) => !isActive,
-    renderNothing
-  )
-)(ContextualDelete)
+export default ContextualDeleteContainer;
